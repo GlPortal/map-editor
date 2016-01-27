b0cb34ef44f370ba8639a9bbc0bf1a3ede16d420
SGOrava <sgd.orava@gmail.com>
2016-01-27 19:37:43 +0100

    Show object panel only if selected object is GlPortal object

--- glportal-editor/ObjectPanel.py
+++ glportal-editor/ObjectPanel.py
@@ -8,41 +8,44 @@ class ObjectPanel(bpy.types.Panel):
   bl_region_type = 'WINDOW'
   bl_context = "object"
 
+  @classmethod
+  def poll(self, context):
+    if context.object.glpTypes != "none":
+      return True
+    return False
+
   def draw(self, context):
     object = context.active_object
     layout = self.layout
     row = layout.row()
 
-    if object.glpTypes != "none":
-      layout.prop(object, "glpTypes")
-      if object.glpTypes == "trigger":
-        layout.prop(object, "glpTriggerTypes")
-      elif object.glpTypes == "wall":
-        layout.prop(object, "glpWallTypes")
-      elif object.glpTypes == "volume":
-        layout.prop(object, "glpVolumeTypes")
+    layout.prop(object, "glpTypes")
+    if object.glpTypes == "trigger":
+      layout.prop(object, "glpTriggerTypes")
+    elif object.glpTypes == "wall":
+      layout.prop(object, "glpWallTypes")
+    elif object.glpTypes == "volume":
+      layout.prop(object, "glpVolumeTypes")
 
-      if object.glpMaterial != "none":
-        mat = MaterialManager.materials[object.glpMaterial]
+    if object.glpMaterial != "none":
+      mat = MaterialManager.materials[object.glpMaterial]
 
-        layout.label(text="Material properties", icon='MATERIAL')
+      layout.label(text="Material properties", icon='MATERIAL')
 
-        row = layout.row(align=True)
-        row.alignment = 'EXPAND'
-        row.label(text="Name : ")
-        row.label(text=mat['fancyname'])
+      row = layout.row(align=True)
+      row.alignment = 'EXPAND'
+      row.label(text="Name : ")
+      row.label(text=mat['fancyname'])
 
-        row = layout.row(align=True)
-        row.alignment = 'EXPAND'
-        row.label(text="Portalable : ")
-        if mat['portalable']:
-          row.label(text='Yes')
-        else:
-          row.label(text='No')
+      row = layout.row(align=True)
+      row.alignment = 'EXPAND'
+      row.label(text="Portalable : ")
+      if mat['portalable']:
+        row.label(text='Yes')
+      else:
+        row.label(text='No')
 
-        row = layout.row(align=True)
-        row.alignment = 'EXPAND'
-        row.label(text="Kind : ")
-        row.label(text=mat['kind'])
-    else:
-      layout.label(text="This is no GlPortal object")
+      row = layout.row(align=True)
+      row.alignment = 'EXPAND'
+      row.label(text="Kind : ")
+      row.label(text=mat['kind'])
