c2b0fb25d1b729e340c8ca205e5a7549bf539bcd
SGOrava <sgd.orava@gmail.com>
2016-01-27 13:41:08 +0100

    Shortened id names in MenuAdd

--- glportal-editor/glportalMenuAdd.py
+++ glportal-editor/glportalMenuAdd.py
@@ -3,27 +3,27 @@ import bpy
 def glportal_add_menu(self, context):
   layout = self.layout
 
-  layout.menu("OBJECT_MT_glportal_add_menu_main", icon="WORLD")
+  layout.menu("glpMenu.main", icon="WORLD")
 
   layout.separator()
 
 class GlPortalAddMenuMain(bpy.types.Menu):
-  bl_idname = "OBJECT_MT_glportal_add_menu_main"
+  bl_idname = "glpMenu.main"
   bl_label = "GlPortal"
 
   def draw(self, context):
     layout = self.layout
 
-    layout.menu("OBJECT_MT_glportal_add_menu_walls", icon='MOD_BUILD')
-    layout.menu("OBJECT_MT_glportal_add_menu_volumes", icon='MOD_FLUIDSIM')
-    layout.menu("OBJECT_MT_glportal_add_menu_triggers", icon='MOD_SCREW')
-    layout.menu("OBJECT_MT_glportal_add_menu_lights", icon='LAMP_POINT')
+    layout.menu("glpMenu.walls", icon='MOD_BUILD')
+    layout.menu("glpMenu.volumes", icon='MOD_FLUIDSIM')
+    layout.menu("glpMenu.triggers", icon='MOD_SCREW')
+    layout.menu("glpMenu.lights", icon='LAMP_POINT')
 
     layout.operator("object.camera_add", text="Spawn (Camera)", icon='OUTLINER_OB_CAMERA')
     layout.operator("wm.add_door", text="Exit", icon='MESH_CUBE')
 
 class GlPortalAddMenuWalls(bpy.types.Menu):
-  bl_idname = "OBJECT_MT_glportal_add_menu_walls"
+  bl_idname = "glpMenu.walls"
   bl_label = "Walls"
 
   def draw(self, context):
@@ -33,7 +33,7 @@ class GlPortalAddMenuWalls(bpy.types.Menu):
     layout.operator("wm.add_wall", icon='META_PLANE')
 
 class GlPortalAddMenuTriggers(bpy.types.Menu):
-  bl_idname = "OBJECT_MT_glportal_add_menu_triggers"
+  bl_idname = "glpMenu.triggers"
   bl_label = "Triggers"
 
   def draw(self, context):
@@ -44,7 +44,7 @@ class GlPortalAddMenuTriggers(bpy.types.Menu):
     layout.operator("wm.add_win", icon='MESH_CUBE')
 
 class GlPortalAddMenuVolumes(bpy.types.Menu):
-  bl_idname = "OBJECT_MT_glportal_add_menu_volumes"
+  bl_idname = "glpMenu.volumes"
   bl_label = "Volumes"
 
   def draw(self, context):
@@ -53,7 +53,7 @@ class GlPortalAddMenuVolumes(bpy.types.Menu):
     layout.operator("wm.add_acid", icon='MESH_CUBE')
 
 class GlPortalAddMenuLights(bpy.types.Menu):
-  bl_idname = "OBJECT_MT_glportal_add_menu_lights"
+  bl_idname = "glpMenu.lights"
   bl_label = "Lights"
 
   def draw(self, context):
