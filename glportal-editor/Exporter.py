107b21946830d6b4bbb95a0eaacb590d740b23ce
SGOrava <sgd.orava@gmail.com>
2016-01-30 17:02:12 +0100

    Added storeMaterials function to Exporter

--- glportal-editor/Exporter.py
+++ glportal-editor/Exporter.py
@@ -6,11 +6,21 @@ import math
 import re
 
 from . import mapHelpers
+from .managers import MaterialManager
 
 class Exporter():
   def __init__(self, filePath, d_p = 4):
     self.__filePath = filePath
     self.__d_p = d_p
+
+  def storeMaterials(self, root, mats):
+    materials = ((k, mats[k]) for k in sorted(mats, key=mats.get))
+    materialElement = tree.SubElement(root, "materials")
+
+    for name, index in materials:
+      element = tree.SubElement(materialElement, "mat")
+      element.set("mid", str(index))
+      element.set("name", name)
 
   def storePosition(self, element, object):
     element.set("x", str(round(object.location[0], self.__d_p)))
@@ -48,8 +58,6 @@ class Exporter():
     lightEnergy = lamp.energy
 
     lightElement = tree.SubElement(targetTree, "light")
-    self.storePosition(lightElement, object)
-
     lightElement.set("r", str(round(colorArray[0], self.__d_p)))
     lightElement.set("g", str(round(colorArray[1], self.__d_p)))
     lightElement.set("b", str(round(colorArray[2], self.__d_p)))
@@ -67,6 +75,7 @@ class Exporter():
     objects = context.scene.objects
     root = tree.Element("map")
 
+    self.storeMaterials(root, MaterialManager.prepareExport())
     materialElement = tree.SubElement(root, "materials")
     material1 = tree.SubElement(materialElement, "mat")
     material2 = tree.SubElement(materialElement, "mat")
