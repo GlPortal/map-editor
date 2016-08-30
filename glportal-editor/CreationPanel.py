b0fdf005bbe3d1e00c2ffd5108965051c18d05d2
SGOrava <sgd.orava@gmail.com>
2016-08-30 22:31:35 +0200

    Change labels in Creation panel

--- glportal-editor/CreationPanel.py
+++ glportal-editor/CreationPanel.py
@@ -9,23 +9,24 @@ class CreationPanel(bpy.types.Panel):
 
   def draw(self, context):
     layout = self.layout
+
     layout.label("Objects:")
     layout.operator("glp.add_door", text="Exit", icon='MESH_CUBE')
     layout.operator("glp.search_model", icon='MESH_CUBE')
     layout.operator("glp.search_material", icon='MATERIAL')
 
-    layout.label("Wall:")
+    layout.label("Walls:")
     layout.operator("glp.add_portalable", icon='MESH_PLANE')
     layout.operator("glp.add_wall", icon='META_PLANE')
 
-    layout.label("Volume:")
+    layout.label("Volumes:")
     layout.operator("glp.add_acid", icon='MESH_CUBE')
 
-    layout.label("Trigger:")
+    layout.label("Triggers:")
     layout.operator("glp.add_death", icon='MESH_CUBE')
     layout.operator("glp.add_radiation", icon='RADIO')
     layout.operator("glp.add_win", icon='MESH_CUBE')
 
-    layout.label("Light:")
+    layout.label("Lights:")
     layout.operator("glp.add_light_common", icon='LAMP_POINT')
     layout.operator("glp.add_light_end", icon='LAMP_POINT')
