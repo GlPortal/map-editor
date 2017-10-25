c58a0f016c1dcfb2bbd4d5be6b2489ade2b4c94b
Juraj Oravec <sgd.orava@gmail.com>
2017-10-26 01:32:27 +0200

    Fix operator test
    Signed-off-by: Juraj Oravec <sgd.orava@gmail.com>

--- tests/operators.py
+++ tests/operators.py
@@ -10,74 +10,74 @@ import toGlPortalXml
 class OperatorsTest(unittest.TestCase):
   operatorsAdd = [
     {
-      "action": bpy.ops.glp.trigger_add_win,
+      "action": bpy.ops.radix.trigger_add_win,
       "type": "trigger",
       "trigger": "win"
     },
     {
-      "action": bpy.ops.glp.trigger_add_death,
+      "action": bpy.ops.radix.trigger_add_death,
       "type": "trigger",
       "trigger": "death"
     },
     {
-      "action": bpy.ops.glp.trigger_add_radiation,
+      "action": bpy.ops.radix.trigger_add_radiation,
       "type": "trigger",
       "trigger": "radiation"
     },
     {
-      "action": bpy.ops.glp.wall_add_metal,
+      "action": bpy.ops.radix.wall_add_metal,
       "type": "wall",
       "material": "metal/tiles00x3"
     },
     {
-      "action": bpy.ops.glp.wall_add_portalable,
+      "action": bpy.ops.radix.wall_add_portalable,
       "type": "wall",
       "material": "concrete/wall00"
     },
     {
-      "action": bpy.ops.glp.volume_add_acid,
+      "action": bpy.ops.radix.volume_add_acid,
       "type": "volume",
       "volume": "acid"
     }
   ]
   operatorsSet = [
     {
-      "action": bpy.ops.glp.trigger_set_audio,
+      "action": bpy.ops.radix.trigger_set_audio,
       "type": "trigger",
       "trigger": "audio"
     },
     {
-      "action": bpy.ops.glp.trigger_set_map,
+      "action": bpy.ops.radix.trigger_set_map,
       "type": "trigger",
       "trigger": "map"
     },
     {
-      "action": bpy.ops.glp.trigger_set_win,
+      "action": bpy.ops.radix.trigger_set_win,
       "type": "trigger",
       "trigger": "win"
     },
     {
-      "action": bpy.ops.glp.trigger_set_death,
+      "action": bpy.ops.radix.trigger_set_death,
       "type": "trigger",
       "trigger": "death"
     },
     {
-      "action": bpy.ops.glp.trigger_set_radiation,
+      "action": bpy.ops.radix.trigger_set_radiation,
       "type": "trigger",
       "trigger": "radiation"
     },
     {
-      "action": bpy.ops.glp.wall_set_metal,
+      "action": bpy.ops.radix.wall_set_metal,
       "type": "wall",
       "material": "metal/tiles00x3"
     },
     {
-      "action": bpy.ops.glp.wall_set_portalable,
+      "action": bpy.ops.radix.wall_set_portalable,
       "type": "wall",
       "material": "concrete/wall00"
     },
     {
-      "action": bpy.ops.glp.volume_set_acid,
+      "action": bpy.ops.radix.volume_set_acid,
       "type": "volume",
       "volume": "acid"
     }
@@ -99,14 +99,14 @@ class OperatorsTest(unittest.TestCase):
     self.assertEqual(len(objects), 1, "More than 1 object is selected.\n")
 
     obj = objects[0]
-    self.assertEqual(obj.glpTypes, operator["type"], "Object type is not correct.\n")
+    self.assertEqual(obj.radixTypes, operator["type"], "Object type is not correct.\n")
 
     if (operator["type"] == "trigger"):
-      self.assertEqual(obj.glpTriggerTypes, operator["trigger"], "Trigger type is not correct.\n")
+      self.assertEqual(obj.radixTriggerTypes, operator["trigger"], "Trigger type is not correct.\n")
     elif (operator["type"] == "volume"):
-      self.assertEqual(obj.glpVolumeTypes, operator["volume"], "Volume type is not correct.\n")
+      self.assertEqual(obj.radixVolumeTypes, operator["volume"], "Volume type is not correct.\n")
     elif (operator["type"] == "wall"):
-      self.assertEqual(obj.glpMaterial, operator["material"], "Material is not correct.\n")
+      self.assertEqual(obj.radixMaterial, operator["material"], "Material is not correct.\n")
 
   def testDynamicOperators(self):
     for operator in self.operatorsAdd:
@@ -125,7 +125,7 @@ class OperatorsTest(unittest.TestCase):
     for operator in toGlPortalXml.operators.operatorList:
       print("Checking:", operator["properties"]["bl_idname"])
       self.assertTrue(
-        hasattr(bpy.types, "GLP_OT_" + operator["properties"]["bl_idname"].lstrip("glp.")),
+        hasattr(bpy.types, "RADIX_OT_" + operator["properties"]["bl_idname"].lstrip("radix.")),
         "Operator 'bpy.ops." + operator["properties"]["bl_idname"] + "' does not exist"
       )
 
