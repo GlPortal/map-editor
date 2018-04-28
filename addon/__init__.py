517e47bf484cbabfa3695b45ab3c4f54c9408a1a
Henry Hirsch <henry@w3-net.de>
2018-04-28 09:54:46 +0200

    Move environment initialization to separate file

--- addon/__init__.py
+++ addon/__init__.py
@@ -10,60 +10,8 @@ bl_info = {
   "tracker_url":  "https://github.com/GlPortal/map-editor/issues"
 }
 
-if "bpy" not in locals():
-  from . import types
-  from . import exportRadixFormat
-  from . import importRadixFormat
-  from . import operatorsList
-  from . import OperatorManager
-  from . import mapOperators
-  from . import preferences
-  from . import mapHelpers
-  from . import radixMenuAdd
-  from . import updateTextures
-  from . import lightsOperators
-  from . import Exporter
-  from . import importer
-  from . import operatorHelpers
-  from . import preferencesHelper
-  from . import MPTypes
-  from . import MaterialPanel
-  from .utils import directory
-  from .managers import MaterialManager
-  from .managers import ModelManager
-  from .managers import MapManager
-  from .managers import AudioManager
-  from . import CreationPanel
-  from . import SidePanel
-  from . import ObjectPanel
-else:
-  import importlib
-
-  importlib.reload(types)
-  importlib.reload(exportRadixFormat)
-  importlib.reload(importRadixFormat)
-  importlib.reload(operatorsList)
-  importlib.reload(operators)
-  importlib.reload(mapOperators)
-  importlib.reload(preferences)
-  importlib.reload(mapHelpers)
-  importlib.reload(radixMenuAdd)
-  importlib.reload(updateTextures)
-  importlib.reload(lightsOperators)
-  importlib.reload(Exporter)
-  importlib.reload(importer)
-  importlib.reload(operatorHelpers)
-  importlib.reload(preferencesHelper)
-  importlib.reload(directory)
-  importlib.reload(MaterialManager)
-  importlib.reload(ModelManager)
-  importlib.reload(MPTypes)
-  importlib.reload(MaterialPanel)
-  importlib.reload(MapManager)
-  importlib.reload(AudioManager)
-  importlib.reload(CreationPanel)
-  importlib.reload(SidePanel)
-  importlib.reload(ObjectPanel)
+from past.builtins import execfile
+execfile('environment.py')
 
 import bpy
 blender = bpy
