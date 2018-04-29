94d46af74f320f9505490176e82eb55b44cfa1dd
Henry Hirsch <henry@w3-net.de>
2018-04-29 18:31:54 +0200

    Move main and walls menu to menu package

--- addon/menu/MenuMain.py
+++ addon/menu/MenuMain.py
@@ -13,14 +13,3 @@ class MenuMain(Menu):
     layout.menu("radixMenu.lights", icon='LAMP_POINT')
 
     layout.operator("object.camera_add", text="Spawn (Camera)", icon='OUTLINER_OB_CAMERA')
-
-
-class MenuWalls(Menu):
-  bl_idname = "radixMenu.walls"
-  bl_label = "Walls"
-
-  def draw(self, context):
-    layout = self.layout
-
-    layout.operator("radix.wall_add_portalable", icon='MESH_PLANE', text="Portalable")
-    layout.operator("radix.wall_add_metal", icon='META_PLANE', text="Metal")
